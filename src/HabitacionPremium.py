3e455e344c4b07485016a975577f78053581403c
VeronicacortesIsaza <veronicacortesisaza@gmail.com>
2025-09-03 09:59:16 -0500

    Metodo de la clase

--- src/HabitacionPremium.py
+++ src/HabitacionPremium.py
@@ -2,3 +2,6 @@ from src.Habitacion import Habitacion
 class HabitacionPremium(Habitacion):
     def __init__(self, numero: int):
         super().__init__(numero, "Premium", 450000)
+        
+    def beneficios_exclusivos(self) -> list[str]:
+        return ["Jacuzzi privado", "Minibar incluido", "Vista panorámica"]
